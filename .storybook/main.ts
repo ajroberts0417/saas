24ac016ac59575d1c061d97938125bcaf6646257
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-26 18:35:29 -0400

    Changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import "./storybook.css";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
