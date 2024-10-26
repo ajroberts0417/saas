256527790ecedda5a8e8ef1e14858d3b937de44a
Andrew Roberts <ajroberts0417@gmail.com>
2024-10-26 13:37:56 -0400

    ugh

--- src/components/salespipeline/Shop.tsx
+++ src/components/salespipeline/Shop.tsx
@@ -10,7 +10,7 @@ import {
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { cn } from "@/";
+import { cn } from "@/lib/utils";
 
 export function SidebarDemo() {
     const links = [
