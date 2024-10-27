071b4b3348318dd28160b051a536ac68609f571f
Andrew Roberts <ajroberts0417@gmail.com>
2024-10-27 10:43:02 -0400

    add employee changes

--- src/orgchart/components/Employee.tsx
+++ src/orgchart/components/Employee.tsx
@@ -12,20 +12,15 @@ interface EmployeeComponentProps {
 const EmployeeComponent = ({ employee }: EmployeeComponentProps) => {
   const { name, type, cash, avatarUrl, culture, trouble } = employee;
   return (
-    <GlareCard width={250} className="flex flex-col items-center justify-center text-white">
-      <div className="card w-full shadow-xl">
-        <figure>
-          <Avatar className="w-20 h-20">
-            <AvatarImage src={avatarUrl} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+    <GlareCard width={256} className="flex flex-col items-center justify-center text-white">
+      <div className="card w-full h-[256px] shadow-xl">
+        <figure className="w-full h-1/2 overflow-hidden flex-shrink-0">
+          <img src={avatarUrl} alt={name} className="absolute top-0 left-0 w-full object-cover" />
         </figure>
-        <div className="card-body">
+        <div className="card-body z-50 bg-black bg-opacity-50">
           <h2 className="card-title">
-            {name}
-            <div className="badge badge-secondary">{type}</div>
+            {type}
           </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <div className="badge badge-primary">Cash: ${cash}</div>
             <div className="badge badge-secondary">Culture: {culture}</div>
