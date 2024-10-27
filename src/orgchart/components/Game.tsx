85a7a5e161655ad9ec267d948e2909c3194e5dea
Andrew Roberts <ajroberts0417@gmail.com>
2024-10-26 21:01:40 -0400

    uh add glare card lol

--- src/orgchart/components/Game.tsx
+++ src/orgchart/components/Game.tsx
@@ -1,14 +1,20 @@
 import OrgchartLayout from "../layout/OrgchartLayout";
 import { initialGame } from "../data/game";
+import EmployeeComponent from "./Employee";
 
 const game = initialGame;
 
 const Game = () => {
   const { scoreboard, announcement, tooltip } = game;
+  console.log(game.orgchart[0]);
+  console.log(game)
   return (
-    <OrgchartLayout scoreboard={scoreboard} announcement={announcement} tooltip={tooltip}>
-      <div>Hello World</div>
-    </OrgchartLayout>
+    <div>
+      <EmployeeComponent employee={game.orgchart.normie1} />
+      {/* <OrgchartLayout scoreboard={scoreboard} announcement={announcement} tooltip={tooltip}>
+        <div>Hello World</div>
+      </OrgchartLayout> */}
+    </div>
   );
 };
 
